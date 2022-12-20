90d075e2c7034308f67eb670303883663058af68
STEPHEN ONYEJE <chibunduonyeje1@gmail.com>
2022-12-20 10:15:16 -0600

    Add animation to mobile menu bar

--- js/mobile-menu-function.js
+++ js/mobile-menu-function.js
@@ -1,15 +1,11 @@
-const hamburger = document.getElementById('hamburger-menu')
-const closeMenubar = document.getElementById('mobile-menu-close')
-const menuBar = document.getElementById('mobile-menu-bar')
+const hamburger = document.getElementById('hamburger-menu');
+const closeMenubar = document.getElementById('mobile-menu-close');
+const menuBar = document.getElementById('mobile-menu-bar');
 
-hamburger.addEventListener('click', function(){
- menuBar.style.display = "block"
- if(window.innerWidth>768){
-    menuBar.style.display = "none"
- }
-})
+hamburger.addEventListener('click', () => {
+  menuBar.style.display = 'block';
+});
 
-closeMenubar.addEventListener('click', function(){
-    menuBar.style.display = "none"
-})
-
+closeMenubar.addEventListener('click', () => {
+  menuBar.style.display = 'none';
+});
