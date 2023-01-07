67fe34e103a2995b3c36818d7cddf2b944f59cdd
STEPHEN ONYEJE <chibunduonyeje1@gmail.com>
2023-01-07 20:08:38 +0100

    Add javascript function to assess the input field and display error message

---
+++ assets/js/validator.js
@@ -0,0 +1,33 @@
+const nameField = document.getElementById('fullname');
+const emailField = document.getElementById('email-field');
+const messageField = document.getElementById('message-field');
+const submitButton = document.getElementById('submit');
+
+const nameErrorMsg = document.getElementById('name-error');
+const emailErrorMsg = document.getElementById('email-error');
+const messageError = document.getElementById('message-error');
+const submitErrorMsg = document.getElementById('submit-error');
+
+submitButton.addEventListener('click', () => {
+  if (nameField.value.length === 0) {
+    nameErrorMsg.innerHTML = 'Your Name is required'
+  }else {
+    nameErrorMsg.innerHTML = ''
+  }
+
+  if(emailField.value.length === 0) {
+    emailErrorMsg.innerHTML = 'Your Email is required'
+
+  }else if(emailField.value !== emailField.value.toLowerCase()) {
+    emailErrorMsg.innerHTML = 'Please input your email address in lowercase.'
+  }else {
+    emailErrorMsg.innerHTML = ''
+  }
+
+  if (messageField.value.length === 0) {
+    messageError.innerHTML = 'Your message field is empty'
+  }else {
+    messageError.innerHTML = ''
+  }
+  
+})
