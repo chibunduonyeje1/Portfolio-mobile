05c6a1df05400a3061064ab2326986b03420f185
STEPHEN ONYEJE <chibunduonyeje1@gmail.com>
2023-01-07 20:20:59 +0100

    Fixed Error with linters in JavaScript

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -6,28 +6,25 @@ const submitButton = document.getElementById('submit');
 const nameErrorMsg = document.getElementById('name-error');
 const emailErrorMsg = document.getElementById('email-error');
 const messageError = document.getElementById('message-error');
-const submitErrorMsg = document.getElementById('submit-error');
 
 submitButton.addEventListener('click', () => {
   if (nameField.value.length === 0) {
-    nameErrorMsg.innerHTML = 'Your Name is required'
-  }else {
-    nameErrorMsg.innerHTML = ''
+    nameErrorMsg.innerHTML = 'Your Name is required';
+  } else {
+    nameErrorMsg.innerHTML = '';
   }
 
-  if(emailField.value.length === 0) {
-    emailErrorMsg.innerHTML = 'Your Email is required'
-
-  }else if(emailField.value !== emailField.value.toLowerCase()) {
-    emailErrorMsg.innerHTML = 'Please input your email address in lowercase.'
-  }else {
-    emailErrorMsg.innerHTML = ''
+  if (emailField.value.length === 0) {
+    emailErrorMsg.innerHTML = 'Your Email is required';
+  } else if (emailField.value !== emailField.value.toLowerCase()) {
+    emailErrorMsg.innerHTML = 'Please input your email address in lowercase.';
+  } else {
+    emailErrorMsg.innerHTML = '';
   }
 
   if (messageField.value.length === 0) {
-    messageError.innerHTML = 'Your message field is empty'
-  }else {
-    messageError.innerHTML = ''
+    messageError.innerHTML = 'Your message field is empty';
+  } else {
+    messageError.innerHTML = '';
   }
-  
-})
+});
